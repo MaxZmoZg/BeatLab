0d058e5cc459e20fcb4bcd7a5ee379c05a1611e9
thepicture <85333076+thepicture@users.noreply.github.com>
2022-04-26 21:27:48 +0500

    feat(LinkShortExtensions): implement the shorter way to declare a link

---
+++ BeatLab/Extensions/LinkShortExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Web.Mvc.Html
+{
+    public static class LinkShortExtensions
+    {
+        public static MvcHtmlString ActionLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName, object htmlAttributes)
+        {
+            return htmlHelper.ActionLink(linkText: linkText,
+                                         actionName: actionName,
+                                         controllerName: controllerName,
+                                         routeValues: new { },
+                                         htmlAttributes: HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+        }
+
+    }
+}
