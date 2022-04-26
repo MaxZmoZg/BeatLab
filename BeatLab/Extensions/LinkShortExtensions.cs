20c83dd8b8deda9fc962a71920bba6d7639a9cd4
thepicture <85333076+thepicture@users.noreply.github.com>
2022-04-26 21:43:31 +0500

    improve(LinkShortExtensions): simplify the ActionLink definition

--- BeatLab/Extensions/LinkShortExtensions.cs
+++ BeatLab/Extensions/LinkShortExtensions.cs
@@ -2,7 +2,11 @@
 {
     public static class LinkShortExtensions
     {
-        public static MvcHtmlString ActionLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName, object htmlAttributes)
+        public static MvcHtmlString ActionLink(this HtmlHelper htmlHelper,
+                                               string linkText,
+                                               string actionName,
+                                               string controllerName,
+                                               object htmlAttributes)
         {
             return htmlHelper.ActionLink(linkText: linkText,
                                          actionName: actionName,
@@ -11,5 +15,17 @@
                                          htmlAttributes: HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
         }
 
+        public static MvcHtmlString ActionLink(this HtmlHelper htmlHelper,
+                                               string linkText,
+                                               string controllerAndActionName,
+                                               string @class)
+        {
+            string[] controllerAndActionArray = controllerAndActionName.Split('.');
+            return htmlHelper.ActionLink(linkText: linkText,
+                                         actionName: controllerAndActionArray[1],
+                                         controllerName: controllerAndActionArray[0],
+                                         routeValues: new { },
+                                         htmlAttributes: HtmlHelper.AnonymousObjectToHtmlAttributes(new { @class = @class }));
+        }
     }
 }
