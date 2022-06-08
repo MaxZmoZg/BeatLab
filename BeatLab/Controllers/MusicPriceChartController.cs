ccca796342566baebe5898eeaad73fd9e75c16cf
thepicture <85333076+thepicture@users.noreply.github.com>
2022-06-08 21:32:21 +0500

    feat(Musics): add the chart

---
+++ BeatLab/Controllers/MusicPriceChartController.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace BeatLab.Controllers
+{
+    public class MusicPriceChartController : Controller
+    {
+        private readonly BeatLabDBEntities db = new BeatLabDBEntities();
+
+        // GET: MusicPriceChart
+        public ActionResult Index(int id)
+        {
+            var prices = db.Music.Find(id).Price_Music.ToList();
+            return View(prices);
+        }
+    }
+}
