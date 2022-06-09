5b57dbc96007532068980719ce4624133fafefe5
thepicture <85333076+thepicture@users.noreply.github.com>
2022-06-09 23:12:08 +0500

    feat(Chat): add the ability to chat

--- BeatLab/Controllers/СhatController.cs
+++ BeatLab/Controllers/СhatController.cs
@@ -1,136 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
-using BeatLab;
 
 namespace BeatLab.Controllers
 {
-    public class СhatController : Controller
+    public class ChatController : Controller
     {
-        private BeatLabDBEntities db = new BeatLabDBEntities();
+        private readonly BeatLabDBEntities db = new BeatLabDBEntities();
 
-        // GET: Сhat
-        public ActionResult Index()
+        // GET: Chat
+        public ActionResult Index(int? inputReceiverId)
         {
-            var сhat = db.Сhat.Include(с => с.User).Include(с => с.User1);
-            return View(сhat.ToList());
-        }
-
-        // GET: Сhat/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
+            int receiverId;
+            if (!inputReceiverId.HasValue)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                receiverId = db.User
+                    .Where(u => u.Login != HttpContext.User.Identity.Name)
+                    .First().ID_User;
             }
-            Сhat сhat = db.Сhat.Find(id);
-            if (сhat == null)
+            else
             {
-                return HttpNotFound();
+                receiverId = db.User.Find(inputReceiverId).ID_User;
             }
-            return View(сhat);
+            int senderId = db.User.First(u => u.Login == HttpContext.User.Identity.Name).ID_User;
+            ViewBag.Chat = db.Chat.Where(c => c.ID_Sender == senderId
+                                              && c.ID_Receiver == receiverId
+                                              || c.ID_Sender == receiverId
+                                              && c.ID_Receiver == senderId);
+            ViewBag.ReceiverId = receiverId;
+            return View(db);
         }
 
-        // GET: Сhat/Create
-        public ActionResult Create()
-        {
-            ViewBag.ID_User1 = new SelectList(db.User, "ID_User", "Last_Name_User");
-            ViewBag.ID_User2_ = new SelectList(db.User, "ID_User", "Last_Name_User");
-            return View();
-        }
-
-        // POST: Сhat/Create
-        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
-        // сведения см. в разделе https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID_Chat,ID_User1,ID_User2_,IsFirstTyping_Chat,Message_Char")] Сhat сhat)
+        public ActionResult PostComment(string text, int receiverId, int senderId)
         {
-            if (ModelState.IsValid)
+            if (!string.IsNullOrWhiteSpace(text))
             {
-                db.Сhat.Add(сhat);
+                Chat chat = new Chat
+                {
+                    ID_Receiver = receiverId,
+                    ID_Sender = senderId,
+                    Message = text
+                };
+                db.Chat.Add(chat);
                 db.SaveChanges();
-                return RedirectToAction("Index");
             }
-
-            ViewBag.ID_User1 = new SelectList(db.User, "ID_User", "Last_Name_User", сhat.ID_User1);
-            ViewBag.ID_User2_ = new SelectList(db.User, "ID_User", "Last_Name_User", сhat.ID_User2_);
-            return View(сhat);
-        }
-
-        // GET: Сhat/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Сhat сhat = db.Сhat.Find(id);
-            if (сhat == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.ID_User1 = new SelectList(db.User, "ID_User", "Last_Name_User", сhat.ID_User1);
-            ViewBag.ID_User2_ = new SelectList(db.User, "ID_User", "Last_Name_User", сhat.ID_User2_);
-            return View(сhat);
-        }
-
-        // POST: Сhat/Edit/5
-        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
-        // сведения см. в разделе https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID_Chat,ID_User1,ID_User2_,IsFirstTyping_Chat,Message_Char")] Сhat сhat)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(сhat).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.ID_User1 = new SelectList(db.User, "ID_User", "Last_Name_User", сhat.ID_User1);
-            ViewBag.ID_User2_ = new SelectList(db.User, "ID_User", "Last_Name_User", сhat.ID_User2_);
-            return View(сhat);
-        }
-
-        // GET: Сhat/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Сhat сhat = db.Сhat.Find(id);
-            if (сhat == null)
-            {
-                return HttpNotFound();
-            }
-            return View(сhat);
-        }
-
-        // POST: Сhat/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Сhat сhat = db.Сhat.Find(id);
-            db.Сhat.Remove(сhat);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            return RedirectToAction("Index", new { inputReceiverId = receiverId });
         }
     }
 }
